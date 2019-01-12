14c07a7fbdb488a69e7441ad3fa3dd6fc259299d
kocc <1748733741@qq.com>
2019-01-12 19:36:19 +0800

    类的用法

--- Dog.py
+++ Dog.py
@@ -1,6 +1,14 @@
 class Dog:
-	def __init__(self):
-		self.name = '旺财'
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
 
-dog = Dog()
+	def __repr__(self):
+		return 'Dog:{}, age:{}'.format(self.name, self.age)
+	
+dog = Dog('旺财', 2)
+
+print(Dog)
+print(dog)
 print(dog.name)
+print(dog.age)
