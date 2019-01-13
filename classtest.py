cf9b5585b24cc3c3eef5d8d8ca84454889f40ab9
kocc <1748733741@qq.com>
2019-01-13 14:42:06 +0800

    继承类并重写方法

--- classtest.py
+++ classtest.py
@@ -1,18 +1,26 @@
 #要求输出
-#ID:101 Name:Jack
-#ID:102 Name:Louplus
+#Jackie's id is 101
+#Louplus's id is 102
 
 class UserData:
 	def __init__(self, ID, Name):
 		self.ID = ID
 		self.Name = Name
 
+class NewUser(UserData):
+	def get_name(self):
+		return self.name
+
+	def set_name(self, value):
+		self.name = value
+
 	def __repr__(self):
-		return 'ID:{} Name:{}'.format(self.ID, self.Name)
+		return '{}\'s ID is {}'.format(self.Name, self.ID)
 
 
 if __name__ == '__main__':
-	user1 = UserData(101, 'Jack')
-	user2 = UserData(102, 'Louplus')
+	user1 = NewUser(101, 'Jack')
+	user1.set_name('Jackie')
+	user2 = NewUser(102, 'Louplus')
 	print(user1)
 	print(user2)
