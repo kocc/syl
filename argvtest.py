213498bc65197052158aabcba138fc981334f4dd
kocc <1748733741@qq.com>
2019-01-20 14:57:38 +0800

    拷贝文件示例

---
+++ argvtest.py
@@ -0,0 +1,17 @@
+#拷贝文件实例
+#argvtest.py
+import sys
+
+def copy_file(src, dst):
+	with open(src, 'r') as src_file:
+		with open(dst, 'w') as dst_file:
+			dst_file.write(src_file.read())
+
+if __name__ == '__main__':
+	if len(sys.argv) == 3:
+		copy_file(sys.argv[1], sys.argv[2])
+	else:
+		print('Parameter Error')
+		print(sys.argv[0], 'srcfile dstfile')
+		sys.exit(-1)
+	sys.exit(0)
